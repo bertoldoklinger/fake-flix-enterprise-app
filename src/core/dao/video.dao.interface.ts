08a65d82ffbb073c786391b5f45e27c1b329ac89
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-04-18 22:13:58 -0300

    fix: removed dao interface from domain

--- src/core/dao/video.dao.interface.ts
+++
@@ -1,7 +0,0 @@
-import { CreateContentData } from '../service/content-management.service';
-
-export interface VideoDAOInterface {
-  create(videoData: CreateContentData): Promise<any>;
-}
-
-export const VideoDAO = Symbol('VideoDao');
