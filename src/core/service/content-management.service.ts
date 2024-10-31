41086f8885d53071c43ddd743f066dfee7c30a12
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-10-31 17:56:17 -0300

    feat: add anti corruption layer

--- src/core/service/content-management.service.ts
+++ src/core/service/content-management.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ExternalMovieClient } from '@src/http/rest/client/external-movie-rating/external-movie-rating.client';
 import { Content } from '@src/persistence/entity/content.entity';
 import { Movie } from '@src/persistence/entity/movie.entity';
 import { Thumbnail } from '@src/persistence/entity/thumbnail.entity';
@@ -16,14 +17,22 @@ export interface CreateMovieData {
 
 @Injectable()
 export class ContentManagementService {
-  constructor(private readonly contentRepository: ContentRepository) {}
+  constructor(
+    private readonly externalMovieClient: ExternalMovieClient,
+    private readonly contentRepository: ContentRepository,
+  ) {}
 
   async createMovie(createMovieData: CreateMovieData): Promise<Content> {
+    const externalRating = await this.externalMovieClient.getRating(
+      createMovieData.title,
+    );
+
     const contentEntity = new Content({
       title: createMovieData.title,
       description: createMovieData.description,
       type: ContentType.MOVIE,
       movie: new Movie({
+        externalRating,
         video: new Video({
           url: createMovieData.url,
           duration: 10,
